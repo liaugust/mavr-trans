c9c9cf7972d58d64b515dd80277426ed846a625e
Ivan Lipatov <ivan.liaugust@gmail.com>
2023-12-07 18:46:06 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // images: {
+  //   domains: [
+  //     "lh3.googleusercontent.com",
+  //     "b2lonoie6htbswwd.public.blob.vercel-storage.com",
+  //     "dvkj4ix5raybn3an.public.blob.vercel-storage.com",
+  //   ],
+  // },
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "b2lonoie6htbswwd.public.blob.vercel-storage.com",
-      "dvkj4ix5raybn3an.public.blob.vercel-storage.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
